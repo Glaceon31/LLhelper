fd03118d3e97b927ea0eb81060b66897304ba1e2
ben1222 <ben_281493815@qq.com>
2018-02-19 22:42:52 +0800

    Move song difficult filter/select logic to llsong.js; Add color to map attribute choice

--- static/llsong.js
+++ static/llsong.js
@@ -57,14 +57,21 @@ function LLSong(songjson, includeDefaultSong) {
    this.songSearchId = 'songsearch';
    this.songDiffId = 'songdiff';
    this.mapAttId = 'map';
+   this.diffSelId = 'diffchoice';
 };
 
+// listeners
 LLSong.prototype.initListeners = function() {
    var me = this;
    var funcOnSongFilterChange = function() { me.onSongFilterChange(); };
+   var funcOnSongSelectChange = function() { me.onSongSelectChange(); };
+   var funcOnDiffSelectChange = function() { me.onDiffSelectChange(); };
    this._listen(this.songAttId, 'change', funcOnSongFilterChange);
    this._listen(this.songUnitId, 'change', funcOnSongFilterChange);
    this._listen(this.songSearchId, 'change', funcOnSongFilterChange);
+   this._listen(this.songDiffId, 'change', funcOnSongFilterChange);
+   this._listen(this.songSelId, 'change', funcOnSongSelectChange);
+   this._listen(this.diffSelId, 'change', funcOnDiffSelectChange);
 };
 LLSong.prototype._listen = function(id, e, func) {
    if (!(id && e && func)) return;
@@ -72,6 +79,8 @@ LLSong.prototype._listen = function(id, e, func) {
    if (!element) return;
    element.addEventListener(e, func);
 };
+
+// gets
 LLSong.prototype.getElementValue = function (id, defaultValue) {
    if (!id) return defaultValue;
    var ret = undefined;
@@ -103,10 +112,21 @@ LLSong.prototype.getSongAttr = function (songindex) {
    if (songattr == "") songattr = this.getElementValue(this.mapAttId, "");
    return songattr;
 };
+
+// event handlers
 LLSong.prototype.defaultOnSongFilterChange = function () {
    this.filterSongs();
+   this.onSongSelectChange();
 };
 LLSong.prototype.onSongFilterChange = LLSong.prototype.defaultOnSongFilterChange;
+LLSong.prototype.defaultOnSongSelectChange = function () {
+   this.filterDiff();
+   this.onDiffSelectChange();
+};
+LLSong.prototype.onSongSelectChange = LLSong.prototype.defaultOnSongSelectChange;
+LLSong.prototype.onDiffSelectChange = function () {};
+
+// filters
 LLSong.prototype.filterSongs = function (songsel, songatt, songunit, songdiff, keyword) {
    // TODO: filter by event(sm, mf, etc.), by star, by special flags (限时, 超难关, 滑键, etc.)
    if (songsel === undefined) songsel = this.getElementOrThrow(this.songSelId);
@@ -151,5 +171,102 @@ LLSong.prototype.filterSongs = function (songsel, songatt, songunit, songdiff, k
 };
 LLSong.prototype.showAllSongs = function (songsel) {
    this.filterSongs(songsel, "", "", "", "");
+};
+LLSong.prototype.filterDiff = function (diffsel, songindex, cnhave, defaultdiff) {
+   if (diffsel === undefined) diffsel = this.getElementOrThrow(this.diffSelId);
+   if (songindex === undefined) songindex = this.getSelectedSongIndex();
+   if (cnhave === undefined) cnhave = '';
+   if (defaultdiff === undefined) defaultdiff = 'expert';
+
+   diffsel.options.length = 0;
+   if (songindex == "") return;
+
+   var savediff = diffsel.value;
+   if (!savediff) savediff = this.getElementValue(this.songDiffId, "");
+   if (!savediff) savediff = defaultdiff;
+
+   var diffname = ['easy', 'normal', 'hard', 'expert', 'master'];
+   var curSong = this.songs[songindex];
+   for (var i in diffname) {
+      var diff = diffname[i];
+      var curSongWithDiff = curSong[diff];
+      if (!curSongWithDiff) continue;
+      if (cnhave == '' || (curSongWithDiff['cnhave'] == cnhave)) {
+         var newOption = new Option(diff, diff);
+         diffsel.options.add(newOption);
+         if (savediff == diff) {
+            diffsel.value = savediff;
+         }
+      }
+   }
+};
+LLSong.prototype.applyDataOfSongWithDiff = function (targets, songindex, diff) {
+   // targets: {
+   //    '<song property>': [
+   //       ['<element id>', '<element property>'], ...
+   //    ], ...
+   // }
+   if (targets === undefined) return;
+   if (songindex === undefined) songindex = this.getSelectedSongIndex();
+   var curSong = this.songs[songindex];
+   if (!curSong) return;
+   if (diff === undefined) diff = this.getElementValue(this.diffSelId, "");
+
+   var songattr = this.getSongAttr(songindex);
+   var songattrcolor = this.attcolor[songattr];
+   for (var songprop in targets) {
+      var value;
+      if (songprop == "attrcolor") {
+         value = songattrcolor;
+      } else if (songprop == "attribute") {
+         value = songattr;
+      } else if (curSong[songprop] !== undefined) {
+         value = curSong[songprop];
+      } else if (curSong[diff] && curSong[diff][songprop] !== undefined) {
+         value = curSong[diff][songprop];
+      } else {
+         console.error("Not found song property '" + songprop + "'");
+         continue;
+      }
+      var elements = targets[songprop];
+      for (var i in elements) {
+         var element = elements[i];
+         if (typeof(element) == "function") {
+            element(value);
+            continue;
+         }
+         if (typeof(element) == "string") element = [element];
+         if (element.length == 0) {
+            console.error("Not found element property data for song property '" + songprop + "'[" + i + "]");
+            continue;
+         }
+         var curObject = document.getElementById(element[0]);
+         if (!curObject) {
+            console.error("Not found element by id '" + element[0] + "'");
+            continue;
+         }
+         if (element.length == 1) {
+            if (songprop == "attrcolor") {
+               curObject.style.color = value;
+            } else {
+               curObject.value = value;
+            }
+         } else {
+            curObject[element[1]] = value;
+         }
+      }
+   }
+};
+
+// utils
+LLSong.prototype.initAttrSelectColor = function (sel) {
+   if (sel === undefined) throw "Not specified select";
+   if (typeof(sel) == "string") sel = this.getElementOrThrow(sel);
+   var len = sel.options.length;
+   for (var i = 0; i < len; i++) {
+      var opt = sel.options[i];
+      opt.style.color = this.attcolor[opt.value];
+   }
+   sel.style.color = this.attcolor[sel.value];
 };
 
