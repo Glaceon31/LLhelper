ae5166451b39a65b10547822ba11562b70467e8b
ben1222 <ben_281493815@qq.com>
2018-11-26 23:11:24 +0800

    merge updateweight_sync.py into updateweight2.py; add url query timeout in updateweight2.py; support live time calculation in updateweight2.py; add songbrief and songdetail api

--- updateweight2.py
+++ updateweight2.py
@@ -32,9 +32,8 @@ STATUS_ERROR = 2
 STATUS_KEYBOARD_INTERRUPT = 3
 STATUS_EXIT = 4
 
-NUMBER_OF_PROCESSES = 20
+NUMBER_OF_THREAD = 20
 
-FILENAME_SUCCESSFUL_LOG = 'success.json'
 FILENAME_SONG_LIST_JSON = 'newsongsjson.txt'
 
 difficultyKeys = ['easy', 'normal', 'hard', 'expert', 'master', 'arcade']
@@ -43,11 +42,10 @@ TITLE_WELCOME = '''# Welcome to LLHelper LLSIF Song Updater!
 
 * Song data based on `%s`.
 * Program originally authored by **Chazeon** and the end of Sept 2017 in NY.
-* Updated by **ben1222** at 2018 July
+* Updated by **ben1222** at 2018 Nov
 
 * We are now updating song list file: `%s`.
-* Song log will be loaded from and written to: `%s`.
-''' % (LLSIF_WIN_API_DOMAIN, FILENAME_SONG_LIST_JSON, FILENAME_SUCCESSFUL_LOG)
+''' % (LLSIF_WIN_API_DOMAIN, FILENAME_SONG_LIST_JSON)
 
 class Note:
     def __init__(self, noteDict):
@@ -68,6 +66,30 @@ class Note:
         if self.isHold():
             weightValue *= NOTE_WEIGHT_HOLD_FACTOR
         return weightValue
+    def getEndTime(self):
+        if self.isHold():
+            return float(self.appearTime) + float(self.effectValue)
+        else:
+            return float(self.appearTime)
+
+class LiveMap:
+    def __init__(self, mapData):
+        self.mapData = mapData
+        positionWeight = [0.0] * 9
+        endTime = 0.0
+        for note in mapData:
+            note = Note(note)
+            position = 9 - note.position
+            positionWeight[position] += note.getNoteWeightedValue()
+            curEndTime = note.getEndTime()
+            if curEndTime > endTime:
+                endTime = curEndTime
+        self.positionWeight = positionWeight
+        self.endTime = endTime
+    def getPositionWeight(self):
+        return map(str, self.positionWeight)
+    def getEndTime(self):
+        return str(self.endTime)
 
 class SyncPrinter:
     def __init__(self):
@@ -82,18 +104,27 @@ def getLiveMapJsonUrl(liveId):
 
 def getLiveMap(liveId):
     liveJsonUrl = getLiveMapJsonUrl(liveId)
-    liveJsonFp = urllib2.urlopen(liveJsonUrl)
+    # timeout for 10 seconds
+    liveJsonFp = urllib2.urlopen(liveJsonUrl, None, 10)
     liveJson = json.load(liveJsonFp)
-    return liveJson
+    return LiveMap(liveJson)
 
-def getPositionWeight(liveId):
-    liveMap = getLiveMap(liveId)
-    positionWeight = [0.0] * 9
-    for note in liveMap:
-        note = Note(note)
-        position = 9 - note.position
-        positionWeight[position] += note.getNoteWeightedValue()
-    return map(str, positionWeight)
+def isLiveDataComplete(live):
+    if not live.has_key('positionweight'):
+        return False
+    if not live.has_key('time'):
+        return False
+    positionWeight = live['positionweight']
+    liveTime = live['time']
+    if len(positionWeight) != 9:
+        return False
+    if len(str(positionWeight[0])) == 0:
+        return False
+    if len(str(liveTime)) == 0:
+        return False
+    if float(liveTime) <= 0:
+        return False
+    return True
 
 class positionWeightUpdateThread (threading.Thread):
     def __init__(self, liveQueue, messageQueue, printer):
@@ -116,16 +147,18 @@ class positionWeightUpdateThread (threading.Thread):
     def updatePositionWeight(self, live):
         liveId = int(live['liveid'])
         try:
-            result = getPositionWeight(liveId)
+            liveMap = getLiveMap(liveId)
+            result = liveMap.getPositionWeight()
+            liveTime = liveMap.getEndTime()
             self.printer.myPrint('* Successfully processed %d' % (liveId))
-            return (liveId, STATUS_SUCCESSFUL, result)
+            return (liveId, STATUS_SUCCESSFUL, result, liveTime)
         except KeyboardInterrupt:
             self.printer.myPrint('* User trying to exit program')
-            return (liveId, STATUS_KEYBOARD_INTERRUPT, None)
+            return (liveId, STATUS_KEYBOARD_INTERRUPT, None, None)
         except Exception as e:
             self.printer.myPrint('* Failed to process %d' % (liveId))
             self.printer.myPrint(e)
-            return (liveId, STATUS_ERROR, None)
+            return (liveId, STATUS_ERROR, None, None)
     def interrupt(self, e):
         if not self.isAlive():
             return
@@ -137,7 +170,7 @@ class positionWeightUpdateThread (threading.Thread):
             self.printer.myPrint('Failed to interrupt thread')
 
 
-def main():
+def main(threadCount):
     songs = {}
     prevLoadedLives = []
     updatedLives = []
@@ -145,6 +178,7 @@ def main():
     messageQueue = Queue.Queue()
     liveQueue = Queue.Queue()
     liveQueueCount = 0
+    totalLiveCount = 0
     threads = []
 
     lives = {}
@@ -152,51 +186,71 @@ def main():
 
     with open(FILENAME_SONG_LIST_JSON, 'r') as f:
         songs = json.load(f)
-    if os.path.exists(FILENAME_SUCCESSFUL_LOG):
-        printer.myPrint('* Succesful log found at `%s`.' % FILENAME_SUCCESSFUL_LOG)
-        with open(FILENAME_SUCCESSFUL_LOG, 'r') as f:
-            prevLoadedLives = json.load(f)
-            printer.myPrint('* %d live maps have previously loaded, skipping...' % len(prevLoadedLives))
-    printer.myPrint('* Skipping lives: %s' % (str(prevLoadedLives)))
+
     for song in songs.values():
         for difficulty_name in [difficulty for difficulty in song.keys() if (difficulty in difficultyKeys)]:
+            totalLiveCount += 1
             live = song[difficulty_name]
             liveId = int(live['liveid'])
-            if liveId not in prevLoadedLives:
+            if not isLiveDataComplete(live):
                 liveQueue.put(live)
                 lives[live['liveid']] = live
                 liveQueueCount += 1
-    try:
-        for i in range(0, NUMBER_OF_PROCESSES):
-            newThread = positionWeightUpdateThread(liveQueue, messageQueue, printer)
-            newThread.start()
-            threads.append(newThread)
-        for i in range(liveQueueCount):
-            liveId, status, result = messageQueue.get()
-            if status == STATUS_SUCCESSFUL:
-                updatedLives.append(liveId)
-                lives[str(liveId)]['positionweight'] = result
 
-    except KeyboardInterrupt:
+    # do not create more thread than number of lives to update
+    if threadCount > liveQueueCount:
+        threadCount = liveQueueCount
+    if threadCount <= 1:
+        printer.myPrint('* Single thread mode, processing %d lives' % liveQueueCount)
+        while not liveQueue.empty():
+            live = liveQueue.get()
+            liveId = int(live['liveid'])
+            try:
+                liveMap = getLiveMap(liveId)
+                live['positionweight'] = liveMap.getPositionWeight()
+                live['time'] = liveMap.getEndTime()
+                updatedLives.append(liveId)
+                printer.myPrint('* Successfully processed %d' % (liveId))
+            except KeyboardInterrupt:
+                printer.myPrint('* User trying to exit program')
+            except Exception as e:
+                printer.myPrint('* Failed to process %d' % (liveId))
+                printer.myPrint(e)
+    else:
+        printer.myPrint('* Multi-thread mode, %d threads processing %d lives' % (threadCount, liveQueueCount))
         try:
-            while True:
-                liveQueue.get(False)
-        except Queue.Empty:
-            printer.myPrint('* Cleared queue *')
-        for curThread in threads:
-            curThread.interrupt(KeyboardInterrupt)
+            for i in range(threadCount):
+                newThread = positionWeightUpdateThread(liveQueue, messageQueue, printer)
+                newThread.start()
+                threads.append(newThread)
+            for i in range(liveQueueCount):
+                liveId, status, result, liveTime = messageQueue.get()
+                if status == STATUS_SUCCESSFUL:
+                    updatedLives.append(liveId)
+                    lives[str(liveId)]['positionweight'] = result
+                    lives[str(liveId)]['time'] = liveTime
+
+        except KeyboardInterrupt:
+            try:
+                while True:
+                    liveQueue.get(False)
+            except Queue.Empty:
+                printer.myPrint('* Cleared queue *')
+            for curThread in threads:
+                curThread.interrupt(KeyboardInterrupt)
 
     with open(FILENAME_SONG_LIST_JSON, 'w') as f:
         json.dump(songs, f, sort_keys=True)
     printer.myPrint('* Successfully processed lives: `%s`.' % (str(updatedLives)))
-    with open(FILENAME_SUCCESSFUL_LOG, 'w') as f:
-        prevLoadedLives.extend(updatedLives)
-        json.dump(prevLoadedLives, f, sort_keys=True)
-    printer.myPrint('* Successful log written to `%s`, %d records written (%d new).' % (FILENAME_SUCCESSFUL_LOG, len(prevLoadedLives), len(updatedLives)))
+    printer.myPrint('* Updated %s , live count = %d (old: %d, new: %d).' % (FILENAME_SONG_LIST_JSON, totalLiveCount, (totalLiveCount - liveQueueCount), len(updatedLives)))
 
     for curThread in threads:
         curThread.join()
 
 if __name__ == '__main__':
     print TITLE_WELCOME
-    main()
+    threadCount = NUMBER_OF_THREAD
+    if len(sys.argv) > 1:
+        threadCount = int(sys.argv[1])
+    main(threadCount)
+
